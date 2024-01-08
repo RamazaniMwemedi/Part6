c4f6dd2cb76498672a4017724f43e2d3b00a6dc4
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2024-01-08 23:48:49 +0300

    Finished 6.3: anecdotes, step1

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
-import { createStore } from "redux";
-import reducer from "./reducer";
-import counterReducer from "./reducer";
+const store = createStore(reducer)
 
-const store = createStore(reducer);
-
-const App = () => {
-  const goodHandler = () => {
-    store.dispatch({
-      type: "GOOD",
-    });
-  };
-  const okayHandler = () => {
-    store.dispatch({
-      type: "OK",
-    });
-  };
-  const badHandler = () => {
-    store.dispatch({
-      type: "BAD",
-    });
-  };
-  const resetStateHandler = () => {
-    store.dispatch({
-      type: "ZERO",
-    });
-  };
-
-  return (
-    <div>
-      <button onClick={goodHandler}>good</button>
-      <button onClick={okayHandler}>ok</button>
-      <button onClick={badHandler}>bad</button>
-      <button onClick={resetStateHandler}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const renderApp = () => {
-  root.render(<App />);
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
