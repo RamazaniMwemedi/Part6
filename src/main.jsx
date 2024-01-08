646202c0d8cced3139fe3f33841b5724e3ecbc68
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2024-01-08 22:20:08 +0300

    Finished 6.2: unicafe revisited, step2

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,41 @@ import ReactDOM from "react-dom/client";
 
 import { createStore } from "redux";
 import reducer from "./reducer";
+import counterReducer from "./reducer";
 
 const store = createStore(reducer);
 
 const App = () => {
-  const good = () => {
+  const goodHandler = () => {
     store.dispatch({
       type: "GOOD",
+    });
+  };
+  const okayHandler = () => {
+    store.dispatch({
+      type: "OK",
+    });
+  };
+  const badHandler = () => {
+    store.dispatch({
+      type: "BAD",
+    });
+  };
+  const resetStateHandler = () => {
+    store.dispatch({
+      type: "ZERO",
     });
   };
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={goodHandler}>good</button>
+      <button onClick={okayHandler}>ok</button>
+      <button onClick={badHandler}>bad</button>
+      <button onClick={resetStateHandler}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   );
 };
